466bd0511ebf77ad94f778afbfd824f7d54ae707
Vishal931167 <vishalrathiya@gmail.com>
2026-08-26 22:05:25 +0530

    Add build.gradle.kts for Android app setup
    This file sets up the Android application with necessary configurations and dependencies.

---
+++ app/src/main/app/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.realwear.scanner"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.realwear.scanner"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+
+    val cameraVersion = "1.3.1"
+    implementation("androidx.camera:camera-core:$cameraVersion")
+    implementation("androidx.camera:camera-camera2:$cameraVersion")
+    implementation("androidx.camera:camera-lifecycle:$cameraVersion")
+    implementation("androidx.camera:camera-view:$cameraVersion")
+
+    implementation("com.google.mlkit:barcode-scanning:17.2.0")
+}
